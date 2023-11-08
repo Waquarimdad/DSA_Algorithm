784833125ddaadb36922fd8b3d245f30b8809a6f
Waquar Imdad <imdad.waquar@epaylater.in>
2023-11-08 10:12:48 +0530

    Added more information about the constructor

--- src/OOPs/Classes/TheoryRelatedToClassAndObjet.java
+++ src/OOPs/Classes/TheoryRelatedToClassAndObjet.java
@@ -19,7 +19,8 @@ public class TheoryRelatedToClassAndObjet {
         // because it's not yet initialized, so in order to print this we need to first initialized the object..
         student = new Student(); // initialize the object, new Student() is called constructor
         // by using the dot operator we can call the instance variable of the object or set the values.
-        student.name = "Waquar Imdad";
+        student.name = "Waquar Khan";
+        student.greeting();
         student.marks = 99.99f;
         student.rollNumbers = 1;
 
@@ -32,13 +33,26 @@ public class TheoryRelatedToClassAndObjet {
         System.out.println(Arrays.toString(arrayOfStudent));
         // this is successful with the help of the constructor that we r creating in the class
         Student newStudent = new Student(
-               1,
-               99.99f,
-               "Salman Khan"
+                "Sarukh Khan",
+                100.0f,
+                7
         );
-        System.out.println(student.name);
-        System.out.println(student.rollNumbers);
-        System.out.println(student.marks);
+        newStudent.greeting();
+        newStudent.changeName("Salman Khan");
+        System.out.println(newStudent.name);
+        System.out.println(newStudent.marks);
+        System.out.println(newStudent.rollNumbers);
+
+        Student otherStudent = new Student(newStudent);
+        System.out.println(otherStudent.name);
+        System.out.println(otherStudent.marks);
+        System.out.println(otherStudent.rollNumbers);
+
+        Student random = new Student();
+        System.out.println(random.name);
+        System.out.println(random.marks);
+        System.out.println(random.rollNumbers);
+
     }
 
     /*
@@ -49,9 +63,10 @@ public class TheoryRelatedToClassAndObjet {
     1. State --> it's value from the data type
     Note: Instance Variable:-> Variable inside an object is known as instance variable or variable declare inside the class..
      (VVImportant) (When we create an object, memory is dynamically allocated at run time...) and return reference to it..
-
      Dynamic Memory Allocation: -> Lef hand side (Student student) = Right hand side (new Student()); whatever in the left hand side of the equal to is happened at compile time
      and the right hand side of the equal to is happened at the runtime.
+
+     Primitive are stored in stack memory, because they r not an object
      */
 }
 // Student student = new Student(); here (Student) is the data type and (student) is the reference variable...
@@ -63,17 +78,52 @@ class Student {
     float marks; // primitive data type
     String name;
 
+    void greeting(){
+        System.out.println("Hello World!, My name is " + name);
+    }
+    void changeName(String newName){
+        name = newName;
+    }
+
     /*
     Constructor is defined what happens when object is created. Constructor is the special type of function in the class.
     It runs when we create an object and it allocates some variables.
-     */
-    Student(int rollNumbers, float marks, String name) {
-        this.rollNumbers = rollNumbers;
-        this.marks = marks;
-        this.name = name;
-    }
-    // this is called by default constructor, created by jvm for us..
-    Student() {
 
+    (this) keyword is replacing with the name of reference variable
+     */
+    // it is called by default constructor, created by jvm for us..
+//    Student() {
+//        this.name = "Abhishek Bacchan";
+//        this.marks = 24.5f;
+//        this.rollNumbers = 43;
+//    }
+    /*
+        It will fail if variable (name) and (name) inside the constructor is same.
+     */
+//    Student(String name, float marks, int rollNumbers) {
+//        name = name;
+//        marks = marks;
+//        rollNumbers = rollNumbers;
+//    }
+    Student () {
+        // this is how we call a constructor from another constructor
+        // internally: new Student("Default Person", 100.0f, 100);
+        // here (this) keyword is referring to the constructor
+        this ("Default Person", 100.0f, 100);
+    }
+
+    /*
+        In order to resolve this we can use two separate variables names or we can use (this) keyword.
+     */
+    Student(String name, float marks, int rollNumbers) {
+        this.name = name;
+        this.marks = marks;
+        this.rollNumbers = rollNumbers;
+    }
+
+    Student (Student other) {
+        this.name = other.name;
+        this.marks = other.marks;
+        this.rollNumbers = other.rollNumbers;
     }
 }
